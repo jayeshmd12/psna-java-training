2b5f3c1713f4d6f21867a3408edcacbbe53a3feb
jayeshmd12 <jayeshmd12@gmail.com>
2025-07-30 21:06:39 +0530

    Create  prime factors.java

---
+++ prime factors.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 2; i <= n; i++) {
+            int count = 0;
+            while (n % i == 0) {
+                count++;
+                n /= i;
+            }
+            if (count > 0) {
+                System.out.println(i + "->" + count);
+            }
+        }
+
+        sc.close();
+    }
+}

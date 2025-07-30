581de6ee1c8c07c6f0e466acc6d29e0aeaa52f9e
jayeshmd12 <jayeshmd12@gmail.com>
2025-07-30 20:46:43 +0530

    Create odd or even .java

---
+++ odd or even .java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        a=sc.nextInt();
+        if((a/2)*2==a){
+            System.out.println("EVEN");
+        }
+        else
+            System.out.println("ODD");
+        
+            
+        }
+}

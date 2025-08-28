a3d8eb955d5dfe51dd0332e315bf79b838ff3c93
jayeshmd12 <jayeshmd12@gmail.com>
2025-08-28 22:06:39 +0530

    Create Check whether given number is perfect square or not.java

---
+++ Check whether given number is perfect square or not.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        if(Math.sqrt(a)% 1==0){
+            System.out.println("Perfect Square");
+            
+        }else{
+            System.out.println("Not");
+        }
+    }
+}

d9a5c7f94ea0a6139b0bab33ad39addc0f5b41b9
jayeshmd12 <jayeshmd12@gmail.com>
2025-08-28 21:25:25 +0530

    Create program to search for a character in a string.java

---
+++ program to search for a character in a string.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        char c=sc.nextLine().charAt(0);
+        String a=sc.nextLine();
+            if(a.contains(String.valueOf(c)))
+            
+                System.out.print("Present");
+            
+            else
+                System.out.print("Not Present");
+               }
+               }

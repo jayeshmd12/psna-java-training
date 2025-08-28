5b3ec36ed7d0b5ed912e3005e30a714304e02568
jayeshmd12 <jayeshmd12@gmail.com>
2025-08-28 22:01:21 +0530

    Create Get two numbers from the user and print the difference between the two numbers.java

---
+++ Get two numbers from the user and print the difference between the two numbers.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1,n2,n3;
+        n1=sc.nextInt();
+        n2=sc.nextInt();
+        n3=n1-n2;
+        System.out.println(n3);
+        
+    }
+    
+}

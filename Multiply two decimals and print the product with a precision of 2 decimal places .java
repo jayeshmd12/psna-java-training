99afbd37835ddf7c575a650746979995e202331d
jayeshmd12 <jayeshmd12@gmail.com>
2025-08-28 22:03:37 +0530

    Create Multiply two decimals and print the product with a precision of 2 decimal places .java

---
+++ Multiply two decimals and print the product with a precision of 2 decimal places .java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        float n1,n2;
+        n1=sc.nextFloat();
+        n2=sc.nextFloat();
+        float n3=(float) n1 * n2;
+        System.out.printf("%.2f",n3);
+    }
+}

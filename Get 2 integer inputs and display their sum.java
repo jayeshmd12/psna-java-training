6994e2b7f69b81545ba9743e25b80484a247e022
jayeshmd12 <jayeshmd12@gmail.com>
2025-08-28 22:00:02 +0530

    Create Get 2 integer inputs and display their sum.java

---
+++ Get 2 integer inputs and display their sum.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1,n2,n3;
+        n1=sc.nextInt();
+        n2=sc.nextInt();
+        n3=n1+n2;
+        System.out.print("Sum of " + n1 + " and "+ n2 + " is "+n3 );
+    
+    }
+}
